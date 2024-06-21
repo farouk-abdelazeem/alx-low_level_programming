fe939a7ff52837f5fbd3098a543475ee856dfe24
“farouk-abdelazeem” <farouk.abdelazeem@gmail.com>
2024-06-20 18:19:59 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/* a c program hat prints "Programming is like building a multilingual puzzle, followed by a new line. */
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 
 int main(void)
 {
