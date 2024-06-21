854a7dcaf9035bc23352b128107762a473f2095b
“farouk-abdelazeem” <farouk.abdelazeem@gmail.com>
2024-06-20 18:12:15 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <studio.h>
+
+/* a c program hat prints "Programming is like building a multilingual puzzle, followed by a new line. */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle";
+			return (0);
+}
+
