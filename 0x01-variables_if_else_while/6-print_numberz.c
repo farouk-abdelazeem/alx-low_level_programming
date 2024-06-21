12e209eb0a260b8603fa36931768dbe484ed357b
“farouk-abdelazeem” <farouk.abdelazeem@gmail.com>
2024-06-21 13:09:28 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char digit = '1';
+
+while (digit < '10')
+{
+putchar(digit);
+digit++;
+}
+putchar('\n');
+
+	return (0);
+}
