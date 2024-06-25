6f0f312e22596e51d1ad8ec9556de22a03375b3b
“farouk-abdelazeem” <farouk.abdelazeem@gmail.com>
2024-06-25 14:42:21 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_sign - prints the sign of a number
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		putchar('-');
+	}
+		return (-1);
+}
