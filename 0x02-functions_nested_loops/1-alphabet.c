04ecb509667b58800e7e9f4be440e60166cc65d5
“farouk-abdelazeem” <farouk.abdelazeem@gmail.com>
2024-06-25 12:44:29 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,24 +4,14 @@
  * print_alphabet
  */
 
-int main ()
-
-{
-
-
-
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char alpha;
 
 for (alpha= 'a'; alpha <= 'z'; alpha++)
 {
-	_putchar(alpha);
+_putchar(alpha);
 }
 _putchar('\n');
 
-}
-
-
-return 0;
 }
