532c162b615cc78da494decb01127f808ad6d519
“farouk-abdelazeem” <farouk.abdelazeem@gmail.com>
2024-06-28 15:54:05 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _isdigit - write a function that check for a digit (0 through 9).
+* @digit: char to check
+* Return:  0 or 1
+**/
+int _isdigit(int digit)
+{
+	if (digit >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
