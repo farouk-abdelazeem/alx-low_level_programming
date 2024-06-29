8feef6c241bba5df79dce1aaa10cd2c563a528cb
“farouk-abdelazeem” <farouk.abdelazeem@gmail.com>
2024-06-29 12:11:00 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*print_numbers - print the numbers 0 to 9 followed by new line.
+*
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
+
